9311c270fb202c2d3004883db60b6afc55ae5d55
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 <wngns0812@gmail.com>
2023-07-19 22:33:50 +0900

    feat: 상품 수정 추가

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,14 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    @Transactional
+    public void updateItem(Long itemid, Book param) {
+        // 영속상태 변경감지로 인하여 따로 update 쿼리를 사용하지 않아도 됨
+        Item findItem = itemRepository.findOne(itemid);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+    }
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
