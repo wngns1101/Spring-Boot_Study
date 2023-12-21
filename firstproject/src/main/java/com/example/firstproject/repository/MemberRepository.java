d0ec34102f53c355e8b705f2ef327cbd6edffb5e
wngns1101 <wngns0812@gmail.com>
2023-12-21 13:35:57 +0900

    feat: 9, 10장 소스코드

--- firstproject/src/main/java/com/example/firstproject/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstproject.repository;
-
-import com.example.firstproject.entity.Member;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
--- firstproject/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into article(id, title, content) values(1, 'gagagaga', '1111');
-insert into article(id, title, content) values(2, 'ananana', '2222');
-insert into article(id, title, content) values(3, 'jajajaja', '3333');
-
-insert into member(id, email, password) values(1, 'wngns@naver.com', '1234');
-insert into member(id, email, password) values(2, 'wngns@gmail.com', '12344');
-insert into member(id, email, password) values(3, 'wngns@kakao.com', '12345');
